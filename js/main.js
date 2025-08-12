4cd0272b8ae9d45f6e3e429027cd831a458ed4fb
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2025-08-12 17:24:15 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,18 +3,33 @@ $(document).ready(function () {
         $(this).next('ul').toggleClass('active');
     })
 
+    // partners slider
     var swiper = new Swiper(".partners__swiper", {
-        slidesPerView: 6,
+        slidesPerView: 3,
         spaceBetween: 4,
         loop: true,
         speed: 700,
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: false,
-        },
+        // autoplay: {
+        //     delay: 5000,
+        //     disableOnInteraction: false,
+        // },
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
+        },
+        breakpoints: {
+            576: {
+                slidesPerView: 3,
+            },
+            768: {
+                slidesPerView: 4,
+            },
+            991: {
+                slidesPerView: 5,
+            },
+            1200: {
+                slidesPerView: 6,
+            },
         },
     });
 
@@ -22,7 +37,7 @@ $(document).ready(function () {
     var swiper = new Swiper(".aidright", {
         loop: true,
         spaceBetween: 20,
-        slidesPerView: 2,
+        slidesPerView: 1,
         // allowTouchMove: false,
         speed: 500,
         // autoplay: {
@@ -36,6 +51,22 @@ $(document).ready(function () {
         pagination: {
             el: ".swiper-pagination.aidpag",
             type: "fraction",
+        },
+        breakpoints: {
+            575: {
+                slidesPerView: 1,
+            },
+
+            820: {
+                slidesPerView: 1.2,
+            },
+
+            991: {
+                slidesPerView: 1.5,
+            },
+            1200: {
+                slidesPerView: 2,
+            },
         },
     });
     var swiper2 = new Swiper(".aidleft", {
@@ -75,6 +106,49 @@ $(document).ready(function () {
 
     $(window).on('scroll', checkHeaderScrolled);
     checkHeaderScrolled();
+
+    if ($('.burger2').length) {
+        (function () {
+            var burger2;
+            burger2 = document.querySelector(".burger2");
+            burger2.addEventListener("click", function () {
+                return burger2.classList.toggle("on");
+            });
+
+        }).call(this);
+    }
+
+    $('.burger2').click(function () {
+        $('.navcol').toggleClass('open');
+    })
+
+    const menuBtn = document.querySelector('.burger2'); 
+    const menu = document.querySelector('.navcol'); 
+    const overlay = document.querySelector('.menu-overlay');
+
+    menuBtn.addEventListener('click', () => {
+        // menu.classList.toggle('active');
+        overlay.classList.toggle('active');
+    });
+
+    overlay.addEventListener('click', () => {
+        menu.classList.remove('open');
+        overlay.classList.remove('active');
+        menuBtn.classList.remove('on');
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 })
 
