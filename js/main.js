4f4ed90356481e997330d011d62cdeed164a91d8
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2025-08-15 11:53:05 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -198,7 +198,10 @@ $(document).ready(function () {
     });
 
     // плеєр
-    const players = Plyr.setup('.player');
+    if ($('.player').length) {
+        const players = Plyr.setup('.player');
+    }
+
 
 
 
@@ -226,74 +229,74 @@ $(document).ready(function () {
     //     focusOnSelect: true,
     //     arrows: false,
     // });
-
-    $('.slider-for').on('init reInit afterChange', function (event, slick, currentSlide) {
-        // currentSlide = індекс (0-based), тому додаємо 1
-        var i = (currentSlide ? currentSlide : 0) + 1;
-        $('.slider-counter').html('<span class="current">' + i + ' </span>/<span class="total"> ' + slick.slideCount + '</span>');
-    });
-
-    $('.slider-for').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: true,
-        fade: true,
-        asNavFor: '.slider-nav',
-        prevArrow: '<button type="button" class="slick-prev"></button>',
-        nextArrow: '<button type="button" class="slick-next"></button>',
-        infinite: false,
-    });
-
-    $('.slider-nav').slick({
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        asNavFor: '.slider-for',
-        focusOnSelect: true,
-        arrows: false,
-        infinite: false,
-        responsive: [
-            {
-                breakpoint: 1441,
-                settings: {
-                    slidesToShow: 1.8,
-                }
-            },
-            {
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow: 1.5,
-                }
-            },
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 1,
-                }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-        ]
-    });
-
-
-    function pauseVideosInInactiveSlides(sliderSelector) {
-        $(sliderSelector).on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-            // Знайти всі відео, крім того, що буде на активному слайді
-            $(this).find('.slick-slide').each(function (index) {
-                if (index !== nextSlide) {
-                    $(this).find('video').each(function () {
-                        this.pause();
-                    });
-                }
-            });
+    if ($('.slider-for').length) {
+        $('.slider-for').on('init reInit afterChange', function (event, slick, currentSlide) {
+            // currentSlide = індекс (0-based), тому додаємо 1
+            var i = (currentSlide ? currentSlide : 0) + 1;
+            $('.slider-counter').html('<span class="current">' + i + ' </span>/<span class="total"> ' + slick.slideCount + '</span>');
         });
+
+        $('.slider-for').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: true,
+            fade: true,
+            asNavFor: '.slider-nav',
+            prevArrow: '<button type="button" class="slick-prev"></button>',
+            nextArrow: '<button type="button" class="slick-next"></button>',
+            infinite: false,
+        });
+
+        $('.slider-nav').slick({
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            asNavFor: '.slider-for',
+            focusOnSelect: true,
+            arrows: false,
+            infinite: false,
+            responsive: [
+                {
+                    breakpoint: 1441,
+                    settings: {
+                        slidesToShow: 1.8,
+                    }
+                },
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 1.5,
+                    }
+                },
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 1,
+                    }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
+        });
+
+
+        function pauseVideosInInactiveSlides(sliderSelector) {
+            $(sliderSelector).on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+                // Знайти всі відео, крім того, що буде на активному слайді
+                $(this).find('.slick-slide').each(function (index) {
+                    if (index !== nextSlide) {
+                        $(this).find('video').each(function () {
+                            this.pause();
+                        });
+                    }
+                });
+            });
+        }
+
+        // Виклик для обох слайдерів
+        pauseVideosInInactiveSlides('.slider-for.aidleft');
+        pauseVideosInInactiveSlides('.slider-nav.aidright');
     }
-
-    // Виклик для обох слайдерів
-    pauseVideosInInactiveSlides('.slider-for.aidleft');
-    pauseVideosInInactiveSlides('.slider-nav.aidright');
-
 })
 
 
