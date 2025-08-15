4a20f021340ffc94dd66b400f92b70f0e1470726
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2025-08-15 11:30:43 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -35,72 +35,72 @@ $(document).ready(function () {
     });
 
     // aid slider
-    var swiper = new Swiper(".aidright", {
-        loop: false,
-        spaceBetween: 20,
-        // slidesPerView: 1,
-        // allowTouchMove: false,
-        speed: 500,
-        // autoplay: {
-        //     delay: 5000,
-        //     disableOnInteraction: false,
-        // },
-        // navigation: {
-        //     nextEl: ".swiper-button-next.garr",
-        //     prevEl: ".swiper-button-prev.garr",
-        // },
-        // pagination: {
-        //     el: ".swiper-pagination.aidpag",
-        //     type: "fraction",
-        // },
-        breakpoints: {
-            575: {
-                slidesPerView: 1,
-            },
+    // var swiper = new Swiper(".aidright", {
+    //     // loop: true,
+    //     spaceBetween: 20,
+    //     // slidesPerView: 1,
+    //     // allowTouchMove: false,
+    //     speed: 500,
+    //     // autoplay: {
+    //     //     delay: 5000,
+    //     //     disableOnInteraction: false,
+    //     // },
+    //     // navigation: {
+    //     //     nextEl: ".swiper-button-next.garr",
+    //     //     prevEl: ".swiper-button-prev.garr",
+    //     // },
+    //     // pagination: {
+    //     //     el: ".swiper-pagination.aidpag",
+    //     //     type: "fraction",
+    //     // },
+    //     breakpoints: {
+    //         575: {
+    //             slidesPerView: 1,
+    //         },
 
-            820: {
-                slidesPerView: 1.2,
-            },
+    //         820: {
+    //             slidesPerView: 1.2,
+    //         },
 
-            991: {
-                slidesPerView: 1.5,
-            },
-            1200: {
-                slidesPerView: 1.8,
-            },
-        },
-    });
-    var swiper2 = new Swiper(".aidleft", {
-        loop: false,
-        spaceBetween: 0,
-        speed: 500,
-        navigation: {
-            nextEl: ".swiper-button-next.garr",
-            prevEl: ".swiper-button-prev.garr",
-        },
-        pagination: {
-            el: ".swiper-pagination.aidpag",
-            type: "fraction",
-        },
-        // navigation: {
-        //     nextEl: ".swiper-button-next",
-        //     prevEl: ".swiper-button-prev",
-        // },
-        // pagination: {
-        //     el: ".swiper-pagination",
-        //     type: "fraction",
-        // },
-        // thumbs: {
-        //     swiper: swiper,
-        // },
-        // autoplay: {
-        //     delay: 5000,
-        //     disableOnInteraction: false,
-        // },
-    });
+    //         991: {
+    //             slidesPerView: 1.5,
+    //         },
+    //         1200: {
+    //             slidesPerView: 1.8,
+    //         },
+    //     },
+    // });
+    // var swiper2 = new Swiper(".aidleft", {
+    //     // loop: true,
+    //     spaceBetween: 0,
+    //     speed: 500,
+    //     navigation: {
+    //         nextEl: ".swiper-button-next.garr",
+    //         prevEl: ".swiper-button-prev.garr",
+    //     },
+    //     pagination: {
+    //         el: ".swiper-pagination.aidpag",
+    //         type: "fraction",
+    //     },
+    //     // navigation: {
+    //     //     nextEl: ".swiper-button-next",
+    //     //     prevEl: ".swiper-button-prev",
+    //     // },
+    //     // pagination: {
+    //     //     el: ".swiper-pagination",
+    //     //     type: "fraction",
+    //     // },
+    //     // thumbs: {
+    //     //     swiper: swiper,
+    //     // },
+    //     // autoplay: {
+    //     //     delay: 5000,
+    //     //     disableOnInteraction: false,
+    //     // },
+    // });
 
-    swiper2.controller.control = swiper;
-    swiper.controller.control = swiper2;
+    // swiper2.controller.control = swiper;
+    // swiper.controller.control = swiper2;
 
 
     // header
@@ -197,7 +197,102 @@ $(document).ready(function () {
         });
     });
 
+    // плеєр
+    const players = Plyr.setup('.player');
 
+
+
+
+
+
+
+
+
+
+    // $('.slider-for').slick({
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     arrows: true,
+    //     fade: true,
+    //     asNavFor: '.slider-nav',
+    //     prevArrow: '<button type="button" class="slick-prev"></button>',
+    //     nextArrow: '<button type="button" class="slick-next"></button>'
+
+    // });
+    // $('.slider-nav').slick({
+    //     slidesToShow: 2,
+    //     slidesToScroll: 1,
+    //     asNavFor: '.slider-for',
+    //     focusOnSelect: true,
+    //     arrows: false,
+    // });
+
+    $('.slider-for').on('init reInit afterChange', function (event, slick, currentSlide) {
+        // currentSlide = індекс (0-based), тому додаємо 1
+        var i = (currentSlide ? currentSlide : 0) + 1;
+        $('.slider-counter').html('<span class="current">' + i + ' </span>/<span class="total"> ' + slick.slideCount + '</span>');
+    });
+
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        asNavFor: '.slider-nav',
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>',
+        infinite: false,
+    });
+
+    $('.slider-nav').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        focusOnSelect: true,
+        arrows: false,
+        infinite: false,
+        responsive: [
+            {
+                breakpoint: 1441,
+                settings: {
+                    slidesToShow: 1.8,
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 1.5,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
+
+    function pauseVideosInInactiveSlides(sliderSelector) {
+        $(sliderSelector).on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+            // Знайти всі відео, крім того, що буде на активному слайді
+            $(this).find('.slick-slide').each(function (index) {
+                if (index !== nextSlide) {
+                    $(this).find('video').each(function () {
+                        this.pause();
+                    });
+                }
+            });
+        });
+    }
+
+    // Виклик для обох слайдерів
+    pauseVideosInInactiveSlides('.slider-for.aidleft');
+    pauseVideosInInactiveSlides('.slider-nav.aidright');
 
 })
 
